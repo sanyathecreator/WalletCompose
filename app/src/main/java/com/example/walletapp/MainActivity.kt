db575ee0dcf4d093db32de87950ea11dd6f07c27
sanyathecreator <olexxxandr.romaniuk@gmail.com>
2025-06-08 00:07:18 +0200

    Add top bar composable with text and profile image

--- app/src/main/java/com/example/walletapp/MainActivity.kt
+++ app/src/main/java/com/example/walletapp/MainActivity.kt
@@ -5,22 +5,52 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.walletapp.ui.theme.TopBar
 import com.example.walletapp.ui.theme.WalletAppTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             WalletAppTheme {
+                val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(
+                    state = rememberTopAppBarState()
+                )
 
+                Scaffold(
+                    modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+                    topBar = {
+                        TopBar(
+                            modifier = Modifier.fillMaxWidth(),
+                            scrollBehavior = scrollBehavior
+                        )
+                    }
+                ) { paddingValues ->
+                    MainScreen(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(paddingValues)
+                    )
+                }
             }
         }
     }
+}
+
+@Composable
+fun MainScreen(modifier: Modifier = Modifier) {
+
 }
